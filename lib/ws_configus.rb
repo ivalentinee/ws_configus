c2576a04120118fab7fe6c195b8eabe88d9b4730
Vlad Shturma <uchiha248@mail.ru>
2013-07-18 10:59:48 +0200

    Created Builder class

--- lib/ws_configus.rb
+++ lib/ws_configus.rb
@@ -2,4 +2,7 @@ require "ws_configus/version"
 
 module WsConfigus
   autoload 'Builder', 'ws_configus/builder'
+
+  def build(env, &block)
+  end
 end
--- spec/lib/builder_spec.rb
+++ spec/lib/builder_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+describe WsConfigus do
+  it "must set key value" do
+    #c = WsConfigus::build :test do
+    WsConfigus::build :test do
+      env :test do
+        name "Petya"
+      end
+    end
+    c.name.must_equal "Petya"
+  end
+end
